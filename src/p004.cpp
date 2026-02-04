6647f9f78aff562511d96d48bd0b1b9c78f5892c
barkbuff <connect.arindamhore@gmail.com>
2026-02-04 23:46:08 +0530

    Upload solution to Problem04

--- src/p004.cpp
+++ src/p004.cpp
@@ -1,6 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+bool isPalindrome(int n) {
+    int original = n, reversed = 0;
+    while (n > 0) {
+        reversed = reversed * 10 + (n % 10);
+        n /= 10;
+    }
+    return original == reversed;
+}
+
+static int solve() {
+    int maxPalindrome = 0;
+
+    for (int i = 999; i >= 100; --i) {
+        for (int j = i; j >= 100; --j) {
+            int product = i * j;
+
+            if (product <= maxPalindrome)
+                break;
+
+            if (isPalindrome(product)) {
+                maxPalindrome = product;
+            }
+        }
+    }
+
+    return maxPalindrome;
+}
+
 int main() {
-    return 0;
+    volatile auto result = solve();
+    (void) result;
 }
